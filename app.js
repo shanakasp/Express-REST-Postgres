8f01a9b1bcbeac5e84e8c42e9279a523591da4c6
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-09 11:38:01 +0530

    modified app.js in .

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ app.get("/", (req, res) => {
 // Mount authRouter under /api/v1/auth
 app.use("/api/v1/auth", authRouter);
 
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(
+    `Time ${timestamp} - API Method - ${req.method} API Path - ${req.path}`
+  );
+  next();
+});
+
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Route not found",
+  });
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
